ce43adfb0b33cd650c52a961a3750b06b76572c4
zhouhansen <z308114274@gmail.com>
2017-10-12 23:21:03 +0800

    1.0.4

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {propertyMatcher, urlMatcher} = require('./lib/urlMatcher')
+const fs = require('fs')
 var defaults = require('./lib/excludeProperties')
 
-var reurl = module.exports = (css, excludeProperties, rewriterFn, done)=>{
+var reurl = module.exports = function (css, excludeProperties, rewriterFn, done){
   if (typeof excludeProperties === 'function') {
     done = rewriterFn
     rewriterFn = excludeProperties
@@ -10,15 +11,29 @@ var reurl = module.exports = (css, excludeProperties, rewriterFn, done)=>{
 
   defaults = defaults.concat(excludeProperties)
 
-  done(propertyMatcher.replace(css, property=>{
-    var propertyName = property.split(':')[0].replace(/^\s+|\s+$/g, '')
+  css = css.toString()
 
-    if (defaults.includes(property)){
-      return property
-    }
+  var properties = propertyMatcher.exec(css)
 
-    return urlMatcher.replace(property, (urlFunc, justURL)=>{
-      return urlFunc.replace(justURL, rewriterFn(justURL))
-    })
-  }).toString())
+  if (properties === null){
+    done(css)
+    return
+  }
+
+  var property = properties[0]
+
+  var propertyName = property.split(':')[0].replace(/^\s+|\s+$/g, '')
+
+  if (defaults.includes(propertyName)){
+    return property
+  }
+
+  var urls = urlMatcher.exec(property)
+  var urlFunc = urls[0]
+  var justUrl = urls[1]
+
+  rewriterFn(justUrl, url=>{
+    var result = css.replace(new RegExp(justUrl, 'g'), url)
+    done(result)
+  })
 }
